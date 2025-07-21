dd71023a00cfff23bc0ce0b02ba090ab64d831c1
Jamie Melton <jasm1@st-andrews.ac.uk>
2025-07-21 17:18:52 +0100

    Created Crate

---
+++ src/lib.rs
@@ -0,0 +1,1 @@
+pub mod operations; // this includes mod.rs
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@
 * !!!This is not my own work!!!
 * This project is a follow along with a course by Ahmed Elsakka on Udemy
 */
+use math_utils::operations::{add::add, multiply::multiply};
+
 fn main() {
-    
+    // create some integer varaibles
+    let a = 3;
+    let b = 4;
+
+    println!("Addition: {}", add(a, b));
+    println!("Multiplication: {}", multiply(a, b));
 }
